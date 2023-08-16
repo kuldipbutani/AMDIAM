40b8c7b62488a2c95934f0d0dcbca876fa6e0980
devanshdesai07 <devansh@rushkar.com>
2023-08-16 19:33:35 +0530

    Changes By Dharmesh

--- AMDIAM/Controllers/ServicesController.cs
+++ AMDIAM/Controllers/ServicesController.cs
@@ -13,5 +13,45 @@ namespace AMDIAM.Controllers
         {
             return View();
         }
+        public ActionResult services1()
+        {
+            return View();
+        }
+        public ActionResult service2()
+        {
+            return View();
+        }
+        public ActionResult service3()
+        {
+            return View();
+        }
+        public ActionResult service4()
+        {
+            return View();
+        }
+        public ActionResult service5()
+        {
+            return View();
+        }
+        public ActionResult service6()
+        {
+            return View();
+        }
+        public ActionResult service7()
+        {
+            return View();
+        }
+        public ActionResult service8()
+        {
+            return View();
+        }
+        public ActionResult service9()
+        {
+            return View();
+        }
+        public ActionResult service10()
+        {
+            return View();
+        }
     }
 }
