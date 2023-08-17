8d0807360f70bfae2cb7f79c308baeaec501ea55
DESKTOP-P1GTRAK\helLO <kuldip.b@rushkar.com>
2023-08-17 15:59:54 +0530

    changes by kd

--- AMDIAM/Controllers/HomeController.cs
+++ AMDIAM/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,6 +20,36 @@ namespace AMDIAM.Controllers
         public ActionResult about()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<string> SendEmailViaSendGrid(string receiverEmail, string subject, string body)
+        {
+            try
+            {
+                var apiKey = "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";
+                var client = new SendGridClient(apiKey);
+
+                var from_email = new EmailAddress("no-reply@rushkar.com", "Rushkar - No Reply");
+                var to_email = new EmailAddress(receiverEmail, receiverEmail);
+                string htmlContent = body;
+
+                var msg = new SendGridMessage()
+                {
+                    From = from_email,
+                    Subject = subject,
+                    HtmlContent = htmlContent
+                };
+                msg.AddTo(to_email);
+                var response = await client.SendEmailAsync(msg);
+
+                return response.StatusCode.ToString();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return e.Message;
+            }
         }
     }
 }
